35b70f199e5fe792c1a2db076d990002d0ed4021
Nardol Darren <nardol.darren@gmail.com>
2025-12-30 12:12:45 +0100

    implement threejs section with three fiber and dei. Values of the canvas can change within zustand implementation

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -10,12 +10,9 @@ function Hero() {
   }, []);
 
   return (
-    <section
-      id="hero"
-      className="mt-[80px] flex flex-col justify-center items-center"
-    >
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-lg md:text-4xl font-bold">MacBook Pro</h1>
+    <section id="hero">
+      <div>
+        <h1>MacBook Pro</h1>
         <Image src="/title.png" alt="MacBook Title" width={800} height={100} />
       </div>
 
@@ -25,12 +22,9 @@ function Hero() {
         autoPlay
         muted
         playsInline
-        className="md:w-[1200px]"
       ></video>
 
-      <button className="bg-blue-500 py-1 px-4 mb-2 rounded-xl text-sm">
-        Buy
-      </button>
+      <button>Buy</button>
 
       <p>From $1599 or $133/mo for 12 months</p>
     </section>
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
 
 export default function Home() {
   return (
     <main>
       <NavBar></NavBar>
       <Hero></Hero>
+      <ProductViewer></ProductViewer>
     </main>
   );
 }
