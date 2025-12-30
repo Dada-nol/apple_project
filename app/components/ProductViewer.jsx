86fe0b76f6e58b10fab9936e309c0c1f27e71982
Nardol Darren <nardol.darren@gmail.com>
2025-12-30 14:52:16 +0100

    change the Boxgeometry with MacbookModel14 and add ambient light

--- app/components/ProductViewer.jsx
+++ app/components/ProductViewer.jsx
@@ -5,6 +5,8 @@ import useMacBookStore from "../store/index";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
+import MacbookModel16 from "./models/Macbook-16";
 
 function ProductViewer() {
   const { color, scale, setColor, setScale } = useMacBookStore();
@@ -63,7 +65,9 @@ function ProductViewer() {
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
-        <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+        <ambientLight intensity={1} />
+        {/* <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} /> */}
+        <MacbookModel14 scale={0.06} position={[-1, 0, 0]} />
 
         <OrbitControls enableZoom={false} />
       </Canvas>
